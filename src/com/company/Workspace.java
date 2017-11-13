d9e38d13d2cb1088c61cf73a29562752985982f5
Eric.Johnson <eric.johnson@astontech.com>
2017-11-13 14:21:40 -0600

    dsfd

---
+++ src/com/company/Workspace.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Workspace {
+
+//    int[]arrival = ;
+//    int[]service = {2,15,14};
+
+    public static void main(String[] args) {
+ // code goes here
+
+    }
+}
