0b82528ff16f7e85f9943ef7bc8469c332d19c1b
Eric.Johnson <eric.johnson@astontech.com>
2018-02-01 14:59:57 -0600

    Directions Burger

--- src/HackerRankAlgos/Staairs.java
+++ src/HackerRankAlgos/Staairs.java
@@ -8,16 +8,15 @@ public class Staairs {
         System.out.println("Enter any Number between 1-100: ");
         Scanner scn = new Scanner(System.in);
         int height = scn.nextInt();
+        createStairs(height);
+    }
+
+    private static void createStairs(int height) {
+
+        StringBuilder sb =new StringBuilder();
 
         for (int i = 0; i < height; i++) {
-            for (int j = 0; j < height; j++) {
-                if (height - i - 2 < j) {
-                    System.out.print("#");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+            System.out.println(sb.append("#"));
         }
     }
 }
